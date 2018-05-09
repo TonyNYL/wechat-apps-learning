7b3bd8b53b8ca2f86518ef9fd3953f9a5f3cf434
宁原隆 <3216099667@qq.com>
2018-05-09 20:59:02 +0800

    数据绑定

--- pages/post/post.js
+++ pages/post/post.js
@@ -5,14 +5,57 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    // date: "Jan 28 2017",
+    // title: "小时候的冰棍儿与雪糕",
+    // postImg: "/images/post/post-4.jpg",
+    // avatar: "/images/avatar/avatar-5.png",
+    // content: "冰棍与雪糕绝不是同一个东西。3到5毛钱的雪糕犹如现在的哈根达斯，而5分1毛的冰棍儿就像现在的老冰棍。时过境迁，...",
+    // readingNum: 92,
+    // collectionNum: 108,
+    // commentNum: 7
+
+    // object: {
+    //   date: "Jan 28 2017"
+    // },
+    // title: "小时候的冰棍儿与雪糕",
+    // postImg: "/images/post/post-4.jpg",
+    // avatar: "/images/avatar/avatar-5.png",
+    // content: "冰棍与雪糕绝不是同一个东西。3到5毛钱的雪糕犹如现在的哈根达斯，而5分1毛的冰棍儿就像现在的老冰棍。时过境迁，...",
+    // readingNum: 92,
+    // collectionNum: {
+    //   array:[108]
+    // },
+    // commentNum: 7
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function () {
     console.log("onLoad:页面被加载");
+
+    // this.setData({
+    //   "title": "一根雪糕的经济学原理",
+    //   "collectionNum.array[0]": 66,
+    //   "object.date": "Jan 28 2019"
+    // })
+    var iceCreamData = {
+      object: {
+        date: "Jan 28 2017"
+      },
+      title: "小时候的冰棍儿与雪糕",
+      postImg: "/images/post/post-4.jpg",
+      avatar: "/images/avatar/avatar-5.png",
+      content: "冰棍与雪糕绝不是同一个东西。3到5毛钱的雪糕犹如现在的哈根达斯，而5分1毛的冰棍儿就像现在的老冰棍。时过境迁，...",
+      readingNum: 92,
+      collectionNum: {
+        array: [108]
+      },
+      commentNum: 7
+    }
+    this.setData({
+      postData: iceCreamData
+    })
   },
 
   /**
