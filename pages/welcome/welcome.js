f19012b601559679c685f95167635945b784f0e6
宁原隆 <3216099667@qq.com>
2018-05-09 21:54:03 +0800

    列表渲染wx:for和页面跳转

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -1,3 +1,16 @@
 Page({
-  
+  onTapJump:function(event){
+    wx.redirectTo({
+      url: '../post/post',
+      success:function(){
+        console.log("jump success")
+      },
+      fail:function(){
+        console.log("fail success")
+      },
+      complete: function () {
+        console.log("complete success")
+      }
+    });
+  }
 })
